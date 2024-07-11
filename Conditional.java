5dabb93692bcd61f23e9eef12c07a09e6631dee3
shankar <gaddamshankar2002@gmail.com>
2024-07-11 20:27:27 +0530

    first commit

---
+++ Conditional.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Conditional {
+	public static void main(String args[])
+	{
+int a=121;
+String result;
+result=(a%10==a/100 4
+)?"polyndrome":"notpolyndrome";
+System.out.println(result);
+}
+}
